8c189b26ca8b5a058573cc3a60803be7565c88ff
Developer <Developer@gmail.com>
2020-06-16 11:27:32 +0300

    init

---
+++ ContentBlocker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContentBlocker
+//
+//  Created by MacBook Pro 5 on 16.06.2020.
+//  Copyright © 2020 MacBook Pro 5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SafariBlocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  SafariBlocker
+//
+//  Created by MacBook Pro 5 on 16.06.2020.
+//  Copyright © 2020 MacBook Pro 5. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
